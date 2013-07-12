7947873cdac070f8025c1d34c1ddc6a98d1cacb5
blk_jack <jcorcoran+github@gmail.com>
2013-07-12 11:16:42 -0400

    Fix floating window activity result cancellation
    Setting Intent.FLAG_ACTIVITY_NEW_TASK on *all* floating window intents
    will result in Android dropping activity results for non-new task
    activities trying to get results back.
    
    Cheers,
    SlimRoms
    
    Change-Id: Ib125bd03733dc73859f7426150110d88319f7625

--- services/java/com/android/server/am/ActivityRecord.java
+++ services/java/com/android/server/am/ActivityRecord.java
@@ -435,9 +435,8 @@ final class ActivityRecord {
             if (floatingWindow) {
                 intent.setFlags(intent.getFlags() & ~Intent.FLAG_ACTIVITY_TASK_ON_HOME);
                 intent.setFlags(intent.getFlags() & ~Intent.FLAG_ACTIVITY_SINGLE_TOP);
-           intent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                
+      intent.addFlags(Intent.FLAG_ACTIVITY_NO_USER_ACTION);
+               
                 // If this is the mother-intent we make it volatile
                 if (topIntent) {
                     intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
