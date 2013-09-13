nazunamoe/android_frameworks_base

Status Icon Color
PS1: Where's this coming from? QS
PS2: Noob coder, what do ya expect
PS3: Fix the error, use getInt instead of getBoolean, getting weird errors on the other side...
PS4: Remove Clock Color support and use natively STATUS_ICON_COLOR instead
PS5: derp

TODO:

- Make CircleBattery into the Status Icon Color support

Credits to:

- ParanoidAndroid
- travp624

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -345,6 +345,9 @@ public class PhoneStatusBar extends BaseStatusBar {
     // tracking calls to View.setSystemUiVisibility()
     int mSystemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE;
 
+    int defColor = 0;
+    int color = 0;
+
     DisplayMetrics mDisplayMetrics = new DisplayMetrics();
 
     // XXX: gesture research
@@ -1152,6 +1155,14 @@ public class PhoneStatusBar extends BaseStatusBar {
     public void addIcon(String slot, int index, int viewIndex, StatusBarIcon icon) {
         if (SPEW) Slog.d(TAG, "addIcon slot=" + slot + " index=" + index + " viewIndex=" + viewIndex
                 + " icon=" + icon);
+
+        Drawable iconDrawable = StatusBarIconView.getIcon(mContext, icon);
+        if (defColor == 0) {
+            iconDrawable.clearColorFilter();
+        } else if (defColor == 1) {
+            iconDrawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        }
+
         StatusBarIconView view = new StatusBarIconView(mContext, slot, null);
         view.set(icon);
         mStatusIcons.addView(view, viewIndex, new LinearLayout.LayoutParams(mIconSize, mIconSize));
@@ -1162,6 +1173,14 @@ public class PhoneStatusBar extends BaseStatusBar {
             StatusBarIcon old, StatusBarIcon icon) {
         if (SPEW) Slog.d(TAG, "updateIcon slot=" + slot + " index=" + index + " viewIndex=" + viewIndex
                 + " old=" + old + " icon=" + icon);
+
+        Drawable iconDrawable = StatusBarIconView.getIcon(mContext, icon);
+        if (defColor == 0) {
+            iconDrawable.clearColorFilter();
+        } else if (defColor == 1) {
+            iconDrawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        }
+
         StatusBarIconView view = (StatusBarIconView)mStatusIcons.getChildAt(viewIndex);
         view.set(icon);
     }
@@ -3049,6 +3068,10 @@ public class PhoneStatusBar extends BaseStatusBar {
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.HIDE_STATUSBAR), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_ICON_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ICON_COLOR_STYLE), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RIBBON_TARGETS_SHORT[AokpRibbonHelper.NOTIFICATIONS]), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.RIBBON_TARGETS_LONG[AokpRibbonHelper.NOTIFICATIONS]), false, this);
@@ -3124,6 +3147,12 @@ public class PhoneStatusBar extends BaseStatusBar {
 
         mClockActions[doubleClick] = Settings.System.getString(cr,
                 Settings.System.NOTIFICATION_CLOCK[doubleClick]);
+
+        color = Settings.System.getInt(cr,
+                Settings.System.STATUS_ICON_COLOR, 0);
+
+        defColor = Settings.System.getInt(cr,
+                Settings.System.ICON_COLOR_STYLE, 0);
 
         if (mClockActions[shortClick]  == null ||mClockActions[shortClick].equals("")) {
             mClockActions[shortClick] = "**clockoptions**";

pcexpress/frameworks_base

Conflicts:
	core/java/android/provider/Settings.java
	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
	packages/SystemUI/src/com/android/systemui/statusbar/policy/SbBatteryController.java
Change-Id: Ie18d3de94512bbe2e6f7e789f2b87358267f5e20

packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -327,6 +327,9 @@ public class PhoneStatusBar extends BaseStatusBar {
     // tracking calls to View.setSystemUiVisibility()
     int mSystemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE;
 
+    private int customColor;
+    private int color = 0;
+
     DisplayMetrics mDisplayMetrics = new DisplayMetrics();
 
     // XXX: gesture research
@@ -401,6 +404,10 @@ public class PhoneStatusBar extends BaseStatusBar {
                     Settings.System.EXPANDED_DESKTOP_STATE), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.NOTIFICATION_SETTINGS_BUTTON), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.STATUS_ICON_COLOR), false, this);
+            resolver.registerContentObserver(Settings.System.getUriFor(
+                    Settings.System.ICON_COLOR_BEHAVIOR), false, this);
             update();
         }
 
@@ -426,6 +433,13 @@ public class PhoneStatusBar extends BaseStatusBar {
                     Settings.System.NOTIFICATION_HIDE_CARRIER, 0, UserHandle.USER_CURRENT) != 0;
             boolean notificationSettingsBtn = Settings.System.getInt(
                     resolver, Settings.System.NOTIFICATION_SETTINGS_BUTTON, 0) == 1;
+
+            color = Settings.System.getInt(resolver,
+                    Settings.System.STATUS_ICON_COLOR, 0);
+
+            customColor = Settings.System.getInt(resolver,
+                    Settings.System.ICON_COLOR_BEHAVIOR, 0) == 1;
+
             if (mHasSettingsPanel) {
                 mSettingsButton.setVisibility(notificationSettingsBtn ? View.VISIBLE : View.GONE);
             } else {
@@ -1275,6 +1289,14 @@ if (mHaloActive) mTickerView.setVisibility(View.GONE);
     public void addIcon(String slot, int index, int viewIndex, StatusBarIcon icon) {
         if (SPEW) Slog.d(TAG, "addIcon slot=" + slot + " index=" + index + " viewIndex=" + viewIndex
                 + " icon=" + icon);
+
+        Drawable iconDrawable = StatusBarIconView.getIcon(mContext, icon);
+        if (customColor) {
+            iconDrawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        } else {
+            iconDrawable.clearColorFilter();
+        }
+
         StatusBarIconView view = new StatusBarIconView(mContext, slot, null);
         view.set(icon);
         mStatusIcons.addView(view, viewIndex, new LinearLayout.LayoutParams(mIconSize, mIconSize));
@@ -1286,6 +1308,14 @@ if (mHaloActive) mTickerView.setVisibility(View.GONE);
             StatusBarIcon old, StatusBarIcon icon) {
         if (SPEW) Slog.d(TAG, "updateIcon slot=" + slot + " index=" + index + " viewIndex=" + viewIndex
                 + " old=" + old + " icon=" + icon);
+
+        Drawable iconDrawable = StatusBarIconView.getIcon(mContext, icon);
+        if (customColor) {
+            iconDrawable.setColorFilter(color, PorterDuff.Mode.SRC_IN);
+        } else {
+            iconDrawable.clearColorFilter();
+        }
+
         StatusBarIconView view = (StatusBarIconView)mStatusIcons.getChildAt(viewIndex);
         view.set(icon);
     }