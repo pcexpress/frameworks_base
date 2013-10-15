c408cb2369ad5b17d702f6d4f6572186aff5d5aa
Griffin Millender <griffinn.millender@gmail.com>
2013-10-15 16:24:38 -0500

    Frameworks: Ability to Enable/Disable Smart Wake covers
    Change-Id: Idf298618b49895898222deb94d8097b20cc05516
    
    Conflicts:
    
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2653,6 +2653,12 @@ public final class Settings {
         public static final String ACTIVE_DISPLAY_TIMEOUT = "active_display_timeout";
 
         /**
+         * Whether lid wakes the device
+         * @hide
+         */
+        public static final String LOCKSCREEN_LID_WAKE = "lockscreen_lid_wake";
+
+        /**
          * @deprecated Use {@link android.provider.Settings.Global#LOW_BATTERY_SOUND}
          * instead
          * @hide
