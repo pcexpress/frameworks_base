0a8b23bcd4a94175a2db5362ca95760da63d80f4
pcexpress <pcexpress805@gmail.com>
2013-10-19 11:30:30 -0700

    Revert "Revert "[1/2] Add battery level around unlock ring""
    This reverts commit 6d97b878fba1bbdb9857d25ba186d3624d4427bc.

--- core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
+++ core/java/com/android/internal/widget/multiwaveview/GlowPadView.java
@@ -30,6 +30,8 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.content.res.Resources;
 import android.content.res.TypedArray;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.graphics.Paint;
 import android.graphics.Paint.Align;
@@ -174,6 +176,10 @@ public class GlowPadView extends View {
     private boolean mDragging;
     private int mNewTargetResources;
     private ArrayList<TargetDrawable> mNewTargetDrawables;
+
+    private Paint mArcPaint;
+    private RectF mArcRect;
+    private float mArcAngle = 0f;
 
     private class AnimationBundle extends ArrayList<Tweener> {
         private static final long serialVersionUID = 0xA84D78726F127468L;
@@ -344,6 +350,14 @@ public class GlowPadView extends View {
         mPaintText.setTextSize(res.getDimensionPixelSize(R.dimen.glowpad_notification_font_size));
         mTextRadius = res.getDimensionPixelSize(R.dimen.glowpad_notification_text_radius);
         mMaxTextArcLength = mTextRadius * MAX_TEXT_ARC_RADIANS;
+
+        mArcPaint = new Paint();
+        mArcPaint.setStrokeWidth(10.0f);
+        mArcPaint.setStyle(Paint.Style.STROKE);
+        mArcRect = new RectF(mHandleDrawable.getPositionX() - mHandleDrawable.getWidth()/2,
+                                 mHandleDrawable.getPositionY() - mHandleDrawable.getHeight()/2,
+                                 mHandleDrawable.getPositionX() + mHandleDrawable.getWidth()/2,
+                                 mHandleDrawable.getPositionY() + mHandleDrawable.getHeight()/2);
     }
 
     private int getResourceId(TypedArray a, int id) {
@@ -1305,6 +1319,15 @@ public class GlowPadView extends View {
         }
         mHandleDrawable.draw(canvas);
 
+        if (mArcAngle > 0 && mHandleDrawable.getAlpha() > 0) {
+            mArcRect.set(mHandleDrawable.getPositionX() - mHandleDrawable.getWidth()/3,
+                    mHandleDrawable.getPositionY() - mHandleDrawable.getHeight()/3,
+                    mHandleDrawable.getPositionX() + mHandleDrawable.getWidth()/3,
+                    mHandleDrawable.getPositionY() + mHandleDrawable.getHeight()/3);
+
+            canvas.drawArc(mArcRect, -90, mArcAngle, false, mArcPaint);
+        }
+        
         if (!TextUtils.isEmpty(mHandleText) && mPaintText.getAlpha() != 0) {
             float x = mHandleDrawable.getPositionX();
             float y = mHandleDrawable.getPositionY();
@@ -1331,6 +1354,7 @@ public class GlowPadView extends View {
 
     public void setDrawOuterRing(boolean drawOuterRing) {
         mDrawOuterRing = drawOuterRing;
+
     }
 
     public void setOnTriggerListener(OnTriggerListener listener) {
@@ -1501,5 +1525,10 @@ public class GlowPadView extends View {
             replaceTargetDrawables(mContext.getResources(), existingResId, existingResId);
         }
         return replaced;
+    }
+
+    public void setArc(float angle, int color) {
+        mArcAngle = angle;
+        mArcPaint.setColor(color);
     }
 }
